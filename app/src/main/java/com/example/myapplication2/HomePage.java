21be53f232dc4fac15c7fbac88f9a83ac17a7a6a
SanskrutiY <yelkarsanskruti@gmail.com>
2022-03-10 13:35:31 +0530

    removed https Search option

--- app/src/main/java/com/example/myapplication2/HomePage.java
+++ app/src/main/java/com/example/myapplication2/HomePage.java
@@ -1,9 +1,13 @@
 package com.example.myapplication2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -111,4 +115,26 @@ public class HomePage extends AppCompatActivity {
         intent.putExtra("randomWord", this.randomWord);
         startActivity(intent);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+                Toast.makeText(this, "About", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.notes:
+                Toast.makeText(this, "Notes", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
